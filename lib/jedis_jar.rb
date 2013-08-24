a465721f1fcf66304b45ab5f6f36070e2b608565
Scott Clasen <scott@heroku.com>
2013-08-23 22:52:37 -0700

    0.0.2

--- lib/jedis_jar.rb
+++ lib/jedis_jar.rb
@@ -1,5 +1,11 @@
-require "jedis_jar/version"
+
 
 module JedisJar
   # Your code goes here...
 end
+
+
+require File.expand_path(File.join(File.dirname(__FILE__), '../jars/jedis-2.1.0.jar'))
+require File.expand_path(File.join(File.dirname(__FILE__), '../jars/commons-pool-1.5.5.jar'))
+
+require "jedis_jar/version"
--- lib/jedis_jar/version.rb
+++ lib/jedis_jar/version.rb
@@ -1,4 +1,4 @@
 module JedisJar
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
   JAR_VERSION = '2.1.0'
 end
